baf85fa56e4ccc77d370427bb3a53e8d09d20826
NeuroSmith-nan <hanant.devcodingai.22@gmail.com>
2026-01-21 17:21:54 +0700

    edit port for connect api with frontend

--- Back_end/urls.py
+++ Back_end/urls.py
@@ -15,9 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include       
+from django.urls import path, include  
+from django.http import HttpResponse     
+from django.conf import settings             
+from django.conf.urls.static import static
+
+def home(request):
+    return HttpResponse("Hello! The Backend Server is running correctly.")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("myApp.urls")),
+    path("", home),
 ]
+
+# เพิ่มท่อนนี้ต่อท้าย เพื่อให้โหลดรูปได้ในโหมด DEBUG
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
